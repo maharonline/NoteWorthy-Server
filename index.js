bf200e61c1303c236be2193e3c7c75438f78b2ab
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-07-08 21:51:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors(origin:  "https://note-worthy-client.vercel.app"
-    credentials: true,
-));
+app.use(cors({
+  origin: "https://note-worthy-client.vercel.app",
+  credentials: true
+}));
 app.use(cookieParser())
 
 
