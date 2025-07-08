521646fffcdb87bcd191c88da812e0b7c37ac503
maharonline <hamzaahmad3006@gmail.com>
2025-07-08 11:29:41 +0500

    NoteWOrthy Backend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: "http://localhost:3000",
+app.use(cors({origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(cookieParser())
