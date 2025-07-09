a105afed9ebacd352aa5604f69157dca53bc7c1f
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-07-09 12:30:50 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,25 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDb=()=>{
-    try {
-        mongoose.connect(process.env.MONGO_URI, {
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       dbName: process.env.MONGO_DB_NAME,
-            {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
-    }
-//         mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// }
-        );
-        console.log("MongoDb is Connected Successfully");
-        
-    } catch (error) {
-        console.log("Something Went Wrong While Connecting MongoDB");
-        
-    }
-}
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-export default connectDb
+    console.log("✅ MongoDB Connected Successfully");
+  } catch (error) {
+    console.log("❌ Error connecting to MongoDB:", error.message);
+  }
+};
+
+export default connectDb;
