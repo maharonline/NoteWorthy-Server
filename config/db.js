167f26096b01ac336d5ac22d75fde3a5aa70ee7d
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-07-09 10:27:56 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,14 @@ import mongoose from "mongoose";
 
 const connectDb=()=>{
     try {
+    //     mongoose.connect(process.env.MONGO_URI, {
+    //   dbName: process.env.MONGO_DB_NAME,
+    // }
         mongoose.connect(process.env.MONGO_URI, {
-      dbName: process.env.MONGO_DB_NAME,
-    });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+        );
         console.log("MongoDb is Connected Successfully");
         
     } catch (error) {
