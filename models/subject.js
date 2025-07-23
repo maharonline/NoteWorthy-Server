be42720bcf488ef9a1bb826ad5a146a6502d222c
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-07-23 22:36:49 +0500

    Update subject.js

--- models/subject.js
+++ models/subject.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 const subjectSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true,unique:true },
-    notes:[{type:mongoose.Schema.Types.ObjectId,ref:"Note",unique:true}]
+    notes:[{type:mongoose.Schema.Types.ObjectId,ref:"Note",unique:true}],
+    totalLikes: { type: Number, default: 0 }, 
   },
   { timestamps: true }
 );
 
 export const Subject = mongoose.model("Subject", subjectSchema);
+
