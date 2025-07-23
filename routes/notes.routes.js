fe1e68e293b171b78aea8906695038408a91f50f
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-07-23 22:45:21 +0500

    Update notes.routes.js

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {  createNoteonsubjecttilte, deleteNotes, getNotes, getNotesByUserId, getNotesUploadedByTeacher,, likeNote } from '../controllers/notesControllers.js';
+import {  createNoteonsubjecttilte, deleteNotes, getNotes, getNotesByUserId, getNotesUploadedByTeacher, likeNote } from '../controllers/notesControllers.js';
 import authenticateUser from '../middleware/auth.middleware.js';
 import { upload } from '../middleware/multer.middleware.js';
 
