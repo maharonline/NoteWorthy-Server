3664cc038f9e3cbd6102954b63234877fe181d5f
Hamza Ahmad <132140231+maharonline@users.noreply.github.com>
2025-08-03 22:45:05 +0500

    Update teacherModel.js

--- models/teacherModel.js
+++ models/teacherModel.js
@@ -1,29 +1,68 @@
+// import mongoose, { Schema } from "mongoose";
+
+// const teacherSchema = new Schema(
+//     {
+//         email: { type: String, required: true, unique: true },
+//         userName: { type: String, required: true },
+//         employeeID: {
+//             type: String,
+//             required: true,
+//             unique: true,
+//             validate: {
+//                 validator: function (value) {
+//                     return value.length === 7; // Employee ID لازمی 6 حروف کا ہو
+//                 },
+//                 message: "Employee ID must be exactly 6 characters long"
+//             }
+//         },
+//         department: { type: String, required: true },
+//         password: { type: String, required: true },
+//         photoURL: { type: String, default: "" },
+//         isEmailVerified: { type: Boolean, default: false },
+//         status: { type: String, enum: ["pending", "Approved", "rejected"], default: "pending" },
+//         roles: { type: [String], default: ["Teacher"] },
+//         failedLoginAttempts: { type: Number, default: 0 },
+//         blockUntil: { type: Date, default: null },
+//         createdAt: { type: Date, default: Date.now, expires: 15778800 },
+//         //createdAt: { type: Date, default: Date.now, expires: 300 }, 
+
+//     },
+//     { timestamps: true }
+// );
+
+// export const Teacher = mongoose.model("Teacher", teacherSchema);
+
+
 import mongoose, { Schema } from "mongoose";
 
 const teacherSchema = new Schema(
     {
         email: { type: String, required: true, unique: true },
         userName: { type: String, required: true },
-        employeeID: {
-            type: String,
-            required: true,
-            unique: true,
-            validate: {
-                validator: function (value) {
-                    return value.length === 7; // Employee ID لازمی 6 حروف کا ہو
-                },
-                message: "Employee ID must be exactly 6 characters long"
-            }
-        },
+        // employeeId: {
+        //     type: String,
+        //     required: true,
+        //     unique: true,
+        //     validate: {
+        //         validator: function (value) {
+        //             return value.length === 7;
+        //         },
+        //         message: "Employee ID must be exactly 7 characters long"
+        //     }
+        // },
         department: { type: String, required: true },
         password: { type: String, required: true },
         photoURL: { type: String, default: "" },
         isEmailVerified: { type: Boolean, default: false },
+        jobLetterUrl:{type:String,required:true,default:''},
         status: { type: String, enum: ["pending", "Approved", "rejected"], default: "pending" },
         roles: { type: [String], default: ["Teacher"] },
         failedLoginAttempts: { type: Number, default: 0 },
         blockUntil: { type: Date, default: null },
         createdAt: { type: Date, default: Date.now, expires: 15778800 },
+        isMarkedForDeletion: { type: Boolean, default: false },
+        deletionTimestamp: { type: Date, default: null },
+
         //createdAt: { type: Date, default: Date.now, expires: 300 }, 
 
     },
